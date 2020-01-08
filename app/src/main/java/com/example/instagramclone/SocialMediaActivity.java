1915d8820d191918cbf2ec593a7cd0990955d51f
Erodos93 <58087567+Erodos93@users.noreply.github.com>
2020-01-08 14:23:48 +0100

    Tab Layout Set Up

--- app/src/main/java/com/example/instagramclone/SocialMediaActivity.java
+++ app/src/main/java/com/example/instagramclone/SocialMediaActivity.java
@@ -1,14 +1,32 @@
 package com.example.instagramclone;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.widget.TableLayout;
+
+
+import com.google.android.material.tabs.TabLayout;
 
 public class SocialMediaActivity extends AppCompatActivity {
-
+private Toolbar toolbar;
+private ViewPager viewPager;
+private TabLayout tabLayout;
+private TabAdapter tabAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_social_media);
+        toolbar= findViewById(R.id.my_toolbar);
+        setSupportActionBar(toolbar);
+
+        viewPager=findViewById(R.id.viewPager);
+        tabAdapter=new TabAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(tabAdapter);
+
+        tabLayout= findViewById(R.id.tabLayout);
+        tabLayout.setupWithViewPager(viewPager,false);
     }
 }
